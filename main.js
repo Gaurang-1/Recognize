1d196d6fefa1c39854773b7c21c3ef0ea5135a1b
Gaurang-1 <72252176+Gaurang-1@users.noreply.github.com>
2020-12-16 15:03:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,23 @@ function take_snapshot(){
 console.log("ml5 version:",ml5.version);
 
 classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Pa5FEvqFB/modal.json',model_loaded);
+
+function modelLoaded(){
+    console.log("model loaded");
+}
+
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,resultt);
+}
+
+function resultt(error,result){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById("result_object_name").innerHTML=result[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=result[0].confidence.toFixed(3);
+    }
+} 
